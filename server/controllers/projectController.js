b7fc8d8a3921af8481bb4c7eb667b4b595dc1f73
cmaina <cmaina2@my.centennialcollege.ca>
2025-06-04 07:45:55 -0400

    Initial backend setup with models, routes, and MongoDB connection

---
+++ server/controllers/projectController.js
@@ -0,0 +1,22 @@
+import Project from "../models/project";
+// READ ALL PROJECT
+export const getAllprojects = async (req, res) =>{
+    try{
+        const projects = await Project.find() // db.projects.find() if using mongo shell
+        res.status(200).json(projects);// 200 HTTP status code for success
+    }catch(error){
+        req.status(500).json({message: error.message});// 500 HTTP status code for server errpr
+
+    }
+
+};
+//create new project
+export const createProject = async(req,res) => {
+    try{
+        const project = new Project.find(req.body);
+        await project.save();
+        res.status(201).json(project); 
+    }catch(error){
+        res.status(501).json({message: error.message});
+    }
+} ;
---
+++ server/routes/projectRoutes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { getAllprojects, createProject } from '../controllers/projectController';
+
+const router = express.Router();
+
+//post and get api projects
+router.post('/',createProject);
+router.get ('/',getAllprojects);
+
+export default router;
