22d0a28e3c1710edb8d279d975ad758c7d8301b5
cyrus maina <cmaina2@my.centennialcollege.ca>
2025-07-28 16:38:46 -0400

    added authmiddleware,util

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -4,11 +4,12 @@ import { getAllprojects,
     updateProject,
     deleteProject,
     getProjectById} from '../controllers/projectController.js';
+import authMiddleware from '../middleware/auth.js';
 
 const router = express.Router();
 
 // create new project
-router.post('/', createProject);
+router.post('/',authMiddleware, createProject);
 
 // read all project
 router.get('/', getAllprojects);
@@ -17,9 +18,9 @@ router.get('/', getAllprojects);
 router.get('/:id', getProjectById);
 
 // update project by id
-router.put('/:id', updateProject);
+router.put('/:id',authMiddleware, updateProject);
 
 // delete project by ID
-router.delete('/:id', deleteProject);
+router.delete('/:id',authMiddleware, deleteProject);
 
 export default router;
